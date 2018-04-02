918191cd2fbe768f20bbcc19062960683f3a4d7f
raiwa <info@sarplataygemas.com>
2018-04-02 14:56:01 +0200

    Revert "Revert "To add stepwizard module to checkout_shipping.php""
    This reverts commit 453240c58a62b5c4c0d486a9d1eb94164326798a.

---
+++ includes/modules/content/checkout_shipping/templates/stepwizard.php
@@ -0,0 +1,25 @@
+ <div class="clearfix"></div>
+  
+ <div class="col-sm-<?php echo $content_width; ?>">
+
+  <div class="contentText">
+    <div class="stepwizard">
+      <div class="stepwizard-row">
+        <div class="stepwizard-step">
+          <button type="button" class="btn btn-primary btn-circle">1</button>
+          <p><?php echo CHECKOUT_BAR_DELIVERY; ?></p>
+        </div>
+        <div class="stepwizard-step">
+          <button type="button" class="btn btn-default btn-circle" disabled="disabled">2</button>
+          <p><?php echo CHECKOUT_BAR_PAYMENT; ?></p>
+        </div>
+        <div class="stepwizard-step">
+          <button type="button" class="btn btn-default btn-circle" disabled="disabled">3</button>
+          <p><?php echo CHECKOUT_BAR_CONFIRMATION; ?></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+ </div>
+  
