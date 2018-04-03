d524127dca2fef812db9987d7931d84ee30369a0
raiwa <info@sarplataygemas.com>
2018-04-03 18:55:45 +0200

    checkout shipping address continue button

--- checkout_shipping_address.php
+++ checkout_shipping_address.php
@@ -295,8 +295,8 @@
   }
 ?>
 
-  <div class="buttonSet">
-    <div class="text-right"><?php echo tep_draw_hidden_field('action', 'submit') . tep_draw_button(IMAGE_BUTTON_CONTINUE, 'fa fa-angle-right', null, 'primary', null, 'btn-success'); ?></div>
+  <div class="row">
+    <?php echo $oscTemplate->getContent('checkout_shipping_address'); ?>
   </div>
   
   <div class="clearfix"></div>
---
+++ includes/languages/english/modules/content/checkout_shipping_address/cm_csa_continue_button.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_CONTINUE_BUTTON_TITLE', 'Continue Button');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_CONTINUE_BUTTON_DESCRIPTION', 'Shows the Continue Button on the Checkout Shipping Address Page.');
+
