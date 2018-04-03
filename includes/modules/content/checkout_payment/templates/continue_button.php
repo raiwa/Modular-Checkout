238c28b8e3846a66c0927aab5e64b1c2a8b432bd
raiwa <info@sarplataygemas.com>
2018-04-03 18:45:39 +0200

    css class name fix in checkout payment button module

--- includes/modules/content/checkout_payment/templates/continue_button.php
+++ includes/modules/content/checkout_payment/templates/continue_button.php
@@ -1,4 +1,4 @@
-<div class="col-sm-<?php echo $content_width; ?> cm-cs-continue-button">
+<div class="col-sm-<?php echo $content_width; ?> cm-cp-continue-button">
   <div class="buttonSet">
     <?php echo '<div class="text-' . MODULE_CONTENT_CHECKOUT_PAYMENT_CONTINUE_BUTTON_ALIGNMENT . '">' . tep_draw_button(IMAGE_BUTTON_CONTINUE, 'fa fa-angle-right', null, 'primary', null, 'btn-success') . '</div>'; ?>
   </div>
