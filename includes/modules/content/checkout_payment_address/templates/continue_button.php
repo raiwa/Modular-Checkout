aa8a970072165d3d146b2a793c2e79c4720f1400
raiwa <info@sarplataygemas.com>
2018-04-03 19:06:05 +0200

    checkout shipping address continue button

---
+++ includes/modules/content/checkout_payment_address/templates/continue_button.php
@@ -0,0 +1,5 @@
+<div class="col-sm-<?php echo $content_width; ?> cm-cpa-continue-button">
+  <div class="buttonSet">
+    <div class="text-right"><?php echo tep_draw_hidden_field('action', 'submit') . tep_draw_button(IMAGE_BUTTON_CONTINUE, 'fa fa-angle-right', null, 'primary', null, 'btn-success'); ?></div>
+  </div>
+</div>
