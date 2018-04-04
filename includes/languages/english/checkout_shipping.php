fd2ac0b40ec731e12dddc868340150b5f5ab51cf
raiwa <info@sarplataygemas.com>
2018-04-04 20:02:37 +0200

    checkout shipping shipping methods

--- includes/languages/english/checkout_shipping.php
+++ includes/languages/english/checkout_shipping.php
@@ -19,11 +19,6 @@ define('TABLE_HEADING_SHIPPING_ADDRESS', 'Shipping Address');
 define('TEXT_CHOOSE_SHIPPING_DESTINATION', 'Please choose from your address book where you would like the items to be delivered to.');
 define('TITLE_SHIPPING_ADDRESS', 'Shipping Address:');
 
-define('TABLE_HEADING_SHIPPING_METHOD', 'Shipping Method');
-define('TEXT_CHOOSE_SHIPPING_METHOD', 'Please select the preferred shipping method to use on this order.');
-define('TITLE_PLEASE_SELECT', 'Please Select');
-define('TEXT_ENTER_SHIPPING_INFORMATION', 'This is currently the only shipping method available to use on this order.');
-
 define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
 define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to select the preferred payment method.');
 
---
+++ includes/languages/english/modules/content/checkout_shipping/cm_cs_shipping_methods.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_SHIPPING_METHODS_TITLE', 'Shipping Methods');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_SHIPPING_METHODS_DESCRIPTION', 'Shows the Shipping Methods on the Checkout Shipping Page.');
+  
+define('MODULE_CONTENT_CHECKOUT_SHIPPING_SHIPPING_METHODS_TABLE_HEADING', 'Shipping Method');
+define('MODULE_CONTENT_CHECKOUT_SHIPPING_SHIPPING_METHODS_TEXT_CHOOSE', 'Please select the preferred shipping method to use on this order.');
+define('MODULE_CONTENT_CHECKOUT_SHIPPING_SHIPPING_METHODS_TITLE_PLEASE_SELECT', 'Please Select');
+define('MODULE_CONTENT_CHECKOUT_SHIPPING_SHIPPING_METHODS_TEXT_ENTER_SHIPPING_INFORMATION', 'This is currently the only shipping method available to use on this order.');
+
