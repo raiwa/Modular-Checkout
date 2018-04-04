7c70ce1398401f9608d3d13ad80edbac999668cc
raiwa <info@sarplataygemas.com>
2018-04-04 20:25:01 +0200

    checkout payment payment methods

---
+++ includes/languages/english/modules/content/checkout_payment/cm_cp_payment_methods.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_PAYMENT_METHODS_TITLE', 'Payment Methods');
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_PAYMENT_METHODS_DESCRIPTION', 'Shows the Payment Methods on the Checkout Payment Page.');
+  
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_PAYMENT_METHODS_TABLE_HEADING', 'Payment Method');
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_PAYMENT_METHODS_TEXT_SELECT', 'Please select the preferred payment method to use on this order.');
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_PAYMENT_METHODS_TITLE_PLEASE_SELECT', 'Please Select');
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_PAYMENT_METHODS_TEXT_ENTER_PAYMENT_INFORMATION', 'This is currently the only payment method available to use on this order.');
+
+
