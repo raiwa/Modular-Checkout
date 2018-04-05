f69ceceb8d2adbc548db305c7ae27fe8b1bd6f3a
MOPS-DanCole <dancole@mops.ca>
2018-04-04 21:29:49 -0400

    To add stepwizard module to checkout_payment.php

--- checkout_payment.php
+++ checkout_payment.php
@@ -137,27 +137,6 @@
     <?php echo $oscTemplate->getContent('checkout_payment'); ?>
   </div>
 
-  <div class="clearfix"></div>
-
-  <div class="contentText">
-    <div class="stepwizard">
-      <div class="stepwizard-row">
-        <div class="stepwizard-step">
-          <a href="<?php echo tep_href_link('checkout_shipping.php', '', 'SSL'); ?>"><button type="button" class="btn btn-default btn-circle">1</button></a>
-          <p><a href="<?php echo tep_href_link('checkout_shipping.php', '', 'SSL'); ?>"><?php echo CHECKOUT_BAR_DELIVERY; ?></a></p>
-        </div>
-        <div class="stepwizard-step">
-          <button type="button" class="btn btn-primary btn-circle">2</button>
-          <p><?php echo CHECKOUT_BAR_PAYMENT; ?></p>
-        </div>
-        <div class="stepwizard-step">
-          <button type="button" class="btn btn-default btn-circle" disabled="disabled">3</button>
-          <p><?php echo CHECKOUT_BAR_CONFIRMATION; ?></p>
-        </div>
-      </div>
-    </div>
-  </div>
-
 </div>
 
 </form>
---
+++ includes/languages/english/modules/content/checkout_payment/cm_cp_stepwizard.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_STEPWIZARD_TITLE', 'StepWizard');
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_STEPWIZARD_DESCRIPTION', 'Shows the StepWizard on the Checkout Payment Page.');
+  
+  define('MODULE_CONTENT_CHECKOUT_PAYMENT_STEPWIZARD_TABLE_HEADING', 'Add the StepWizard on your Checkout Payment Page.');
+
