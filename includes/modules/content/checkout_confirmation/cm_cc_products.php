f18181284e7868134c3c85c1e497057dba353ed1
Piernas <mazinfriki@gmail.com>
2018-04-06 00:47:01 +0200

    checkout confirmation - > products CM

---
+++ includes/modules/content/checkout_confirmation/cm_cc_products.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_CONFIRMATION_PRODUCTS_TITLE', 'Products');
+  define('MODULE_CONTENT_CHECKOUT_CONFIRMATION_PRODUCTS_DESCRIPTION', 'Shows Products and order totals on the Checkout Confirmation Page.');
+  define('HEADING_PRODUCTS', 'Products');
