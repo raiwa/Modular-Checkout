c28edb6f87be7d0d2122409fa312eb98bdffabe9
Piernas <mazinfriki@gmail.com>
2018-04-06 12:44:16 +0200

    cc-payment-information

---
+++ includes/languages/english/modules/content/checkout_confirmation/cm_cc_payment_information.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_CONFIRMATION_PAYMENT_INFORMATION_TITLE', 'Payment information');
+  define('MODULE_CONTENT_CHECKOUT_CONFIRMATION_PAYMENT_INFORMATION_DESCRIPTION', 'Shows the Payment information provided by the payment module on the Checkout Confirmation Page.');
+
+
