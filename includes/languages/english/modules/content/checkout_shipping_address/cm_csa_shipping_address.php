f9de6c3ae0446836ec72eecd1bc15cb6ebb61edb
raiwa <info@sarplataygemas.com>
2018-04-07 17:59:59 +0200

    checkout shipping address shipping address

---
+++ includes/languages/english/modules/content/checkout_shipping_address/cm_csa_shipping_address.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_SHIPPING_ADDRESS_TITLE', 'Shipping Address');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_SHIPPING_ADDRESS_DESCRIPTION', 'Shows the Shipping Address on the Checkout Shipping Address Page.');
+
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_SHIPPING_ADDRESS_TABLE_HEADING', 'Shipping Address');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_SHIPPING_ADDRESS_TEXT_SELECTED_SHIPPING_DESTINATION', 'This is the currently selected shipping address where the items in this order will be delivered to.');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_SHIPPING_ADDRESS_TITLE_SHIPPING_ADDRESS', 'Shipping Address:');
+  
