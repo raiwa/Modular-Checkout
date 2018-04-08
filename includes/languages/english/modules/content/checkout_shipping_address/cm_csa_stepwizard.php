080721ecb30acff67b59042c1707a99b9d347605
MOPS-DanCole <dancole@mops.ca>
2018-04-08 14:11:03 -0400

    To add stepwizard module to checkout_shipping_address.php

---
+++ includes/languages/english/modules/content/checkout_shipping_address/cm_csa_stepwizard.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_STEPWIZARD_TITLE', 'StepWizard');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_ADDRESS_STEPWIZARD_DESCRIPTION', 'Shows the StepWizard on your Checkout Shipping Address Page.');
