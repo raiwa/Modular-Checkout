6b87d371bc2b0633138f7211843398cd8b58f7b6
MOPS-DanCole <dancole@mops.ca>
2018-04-08 12:52:02 -0400

    To add stepwizard module to checkout_payment_address.php

--- checkout_payment_address.php
+++ checkout_payment_address.php
@@ -211,28 +211,6 @@
     <?php echo $oscTemplate->getContent('checkout_payment_address'); ?>
   </div>
 
-  <div class="clearfix"></div>
-
-  <div class="contentText">
-    <div class="stepwizard">
-      <div class="stepwizard-row">
-        <div class="stepwizard-step">
-          <button type="button" class="btn btn-default btn-circle" disabled="disabled">1</button>
-          <p><?php echo CHECKOUT_BAR_DELIVERY; ?></p>
-        </div>
-        <div class="stepwizard-step">
-          <button type="button" class="btn btn-primary btn-circle">2</button>
-          <p><?php echo CHECKOUT_BAR_PAYMENT; ?></p>
-        </div>
-        <div class="stepwizard-step">
-          <button type="button" class="btn btn-default btn-circle" disabled="disabled">3</button>
-          <p><?php echo CHECKOUT_BAR_CONFIRMATION; ?></p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
 <?php
   if ($process == true) {
 ?>
---
+++ includes/modules/content/checkout_payment_address/templates/tpl_cm_cpa_stepwizard.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $Id$
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+  Copyright (c) 2018 osCommerce
+  Released under the GNU General Public License
+*/
+?>
+<div class="col-sm-<?php echo $content_width; ?> cm-cpa-stepwizard">
+
+  <div class="contentText">
+    <div class="stepwizard">
+      <div class="stepwizard-row">
+        <div class="stepwizard-step">
+          <button type="button" class="btn btn-default btn-circle" disabled="disabled">1</button>
+          <p><?php echo CHECKOUT_BAR_DELIVERY; ?></p>
+        </div>
+        <div class="stepwizard-step">
+          <button type="button" class="btn btn-primary btn-circle">2</button>
+          <p><?php echo CHECKOUT_BAR_PAYMENT; ?></p>
+        </div>
+        <div class="stepwizard-step">
+          <button type="button" class="btn btn-default btn-circle" disabled="disabled">3</button>
+          <p><?php echo CHECKOUT_BAR_CONFIRMATION; ?></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+  
