4de76a3e7478e083946586df6c1512512b765215
MOPS-DanCole <dancole@mops.ca>
2018-04-08 08:39:41 -0400

    To standardize use of HEADING_TABLE constants in title modules.

--- includes/languages/english/modules/content/checkout_shipping/cm_cs_title.php
+++ includes/languages/english/modules/content/checkout_shipping/cm_cs_title.php
@@ -7,8 +7,7 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_CHECKOUT_SHIPPING_TITLE_TITLE', 'Page Heading');
+  define('MODULE_CONTENT_CHECKOUT_SHIPPING_TITLE_HEADING_TITLE', 'Page Heading');
   define('MODULE_CONTENT_CHECKOUT_SHIPPING_TITLE_DESCRIPTION', 'Shows the Page Heading on the Checkout Confirmation Page.');
-  define('MODULE_CONTENT_CHECKOUT_SHIPPING_TITLE_TABLE_HEADING', 'Add the Page Heading on your Checkout Confirmation Page.');
-  
+   
   define('MODULE_CONTENT_CHECKOUT_SHIPPING_TITLE_PUBLIC_TITLE', 'Delivery Information');
